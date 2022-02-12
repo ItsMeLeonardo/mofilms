ebd499983f05ea9708496cc185bad0efba84ebfa
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-12 18:56:14 -0500

    Fix heigh of container layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,11 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <Container md className="container" css={{ p: ".5rem" }}>
+    <Container
+      md
+      className="container"
+      css={{ p: ".5rem", background: "$gray900", minHeight: "100vh" }}
+    >
       <Navbar />
       <main>{children}</main>
     </Container>
