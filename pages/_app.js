92b5c6b3afdaa1bc83cabd2bd64dcfc282a9df3a
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-12 18:54:41 -0500

    change of theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,20 @@ import { NextUIProvider } from "@nextui-org/react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
+import { createTheme } from "@nextui-org/react";
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      accents1: "#333333",
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
