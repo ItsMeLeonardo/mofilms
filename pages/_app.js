ff9811ea9f6fa25c1bf84fef0e9694d6c966560e
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-12 13:20:54 -0500

    [ADD] nextUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { NextUIProvider } from "@nextui-org/react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
