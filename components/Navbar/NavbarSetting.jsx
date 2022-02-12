bbd78f3f42e4252cbc8c3920c62a0c047a94a74d
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-12 18:57:46 -0500

    add searcher to input

--- components/Navbar/NavbarSetting.jsx
+++ components/Navbar/NavbarSetting.jsx
@@ -1,34 +1,33 @@
-import LinkNext from "next/link";
+import { Grid, Avatar } from "@nextui-org/react";
+import { Notification } from "react-iconly";
 
-import { Grid, Row, Col, Link, Input, Avatar } from "@nextui-org/react";
-import { Search, Notification } from "react-iconly";
+import SearchInput from "../SearchInput";
 
 const defaultAvatar =
   "https://i.pinimg.com/236x/29/68/3b/29683b54520b500a531ad18a4534c85e.jpg";
 
-export default function Navbar() {
+export default function NavbarSetting() {
   return (
     <Grid.Container
-      gap={1}
+      gap={0.5}
+      wrap="nowrap"
       direction="row"
       justify="flex-end"
       alignItems="center"
     >
-      <Grid>
+      <Grid alignItems="center" justify="center">
+        <SearchInput />
+      </Grid>
+      {/* NOTIFICATION */}
+      <Grid xs={0} sm={2} alignItems="center" justify="center">
         <Avatar
           squared
-          icon={<Search size="small" />}
+          icon={<Notification size="small" stroke="bold" primaryColor="#fff" />}
           css={{ cursor: "pointer" }}
         />
       </Grid>
-      <Grid>
-        <Avatar
-          squared
-          icon={<Notification size="small" />}
-          css={{ cursor: "pointer" }}
-        />
-      </Grid>
-      <Grid>
+      {/* AVATAR */}
+      <Grid alignItems="center" justify="center">
         <Avatar
           size="lg"
           src={defaultAvatar}
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -7,7 +7,7 @@ import NavbarLinks from "./NavbarLinks";
 export default function Navbar() {
   return (
     <Grid.Container justify="space-between" alignItems="center">
-      <Grid xs={4} sm={1}>
+      <Grid xs={1} sm={1}>
         <LinkNext href="/">
           <a>
             <div>LOGO</div>
@@ -17,7 +17,7 @@ export default function Navbar() {
       <Grid xs={0} sm={7} gap={2} align="center" justify="flex-start">
         <NavbarLinks />
       </Grid>
-      <Grid xs={8} sm={4}>
+      <Grid xs={11} sm={4}>
         <NavbarSetting />
       </Grid>
     </Grid.Container>
