7bc3260c50f2ace376803f15cbe7f7166dd8aeb8
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-14 17:20:29 -0500

    [FEAT] add overlay to horizontal list

--- components/HorizontalList.jsx
+++ components/HorizontalList.jsx
@@ -3,15 +3,30 @@ import { Grid } from "@nextui-org/react";
 export default function HorizontalList({ children }) {
   return (
     <>
-      <section>
-        <Grid.Container gap={2} wrap="nowrap" css={{ width: "max-content" }}>
-          {children}
-        </Grid.Container>
-      </section>
+      <div className="container">
+        <section className="scroll-content">
+          <Grid.Container gap={2} wrap="nowrap" css={{ width: "max-content" }}>
+            {children}
+          </Grid.Container>
+        </section>
+      </div>
 
       <style jsx>{`
-        section {
+        .scroll-content {
           overflow: auto hidden;
+        }
+        .container {
+          position: relative;
+        }
+        .container::after {
+          content: "";
+          position: absolute;
+          top: 0;
+          right: 0;
+          z-index: 10;
+          width: 5rem;
+          height: 100%;
+          background: linear-gradient(90deg, transparent, #111);
         }
       `}</style>
     </>
