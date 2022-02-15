75181e0ca6e5fb22b463a07df3723b20c92e4242
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-15 12:52:06 -0500

    [FEAT] add actor searcher

--- pages/actors/index.jsx
+++ pages/actors/index.jsx
@@ -1,3 +1,52 @@
+import { Row, Spacer, Text, Grid, Col, Input } from "@nextui-org/react";
+import { Search } from "react-iconly";
+
+import PopularActors from "../../components/Actor/PopularActors";
+import ActorCard from "../../components/Actor/ActorCard";
+
+import { popularActors } from "../../utils/ex";
+
 export default function Actors() {
-  return <h1>Actors</h1>;
+  return (
+    <>
+      <PopularActors />
+      <Spacer />
+
+      <Col align="center" justify="center">
+        <Text h2 css={{ textGradient: "$gradRed" }} weight="bold">
+          Search an Actor
+        </Text>
+
+        <Row align="center" justify="center">
+          <Input
+            type="search"
+            placeholder="Brad Pitt"
+            contentRight={<Search />}
+            fullWidth
+            css={{ maxW: "400px" }}
+          />
+        </Row>
+        <Spacer />
+
+        <Grid.Container
+          gap={4}
+          align="center"
+          justify="flex-start"
+          css={{ w: "100%" }}
+        >
+          {popularActors.map((actor) => (
+            <ActorCard key={actor.id} {...actor} advanced />
+          ))}
+        </Grid.Container>
+      </Col>
+      <style jsx>{`
+      span {
+        position: "absolute",
+        width: "100%",
+        display: "flex",
+        justify-content: "center",
+      }
+        `}</style>
+    </>
+  );
 }
