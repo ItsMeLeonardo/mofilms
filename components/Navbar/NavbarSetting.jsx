f028a772bf991fa310d564513a751a72d6015e17
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-15 19:23:38 -0500

    [FEAT] add profile options

--- components/Navbar/NavbarSetting.jsx
+++ components/Navbar/NavbarSetting.jsx
@@ -1,10 +1,13 @@
-import { Grid, Avatar } from "@nextui-org/react";
+import { Grid, Avatar, Tooltip } from "@nextui-org/react";
 import { Notification } from "react-iconly";
 
 import SearchInput from "../SearchInput";
+import ProfileOptions from "../Profile/ProfileOptions";
 
 const defaultAvatar =
   "https://i.pinimg.com/236x/29/68/3b/29683b54520b500a531ad18a4534c85e.jpg";
+
+const tooltipCss = { bgBlur: ".5rem", bg: "rgba(51,51,51,.65)" };
 
 export default function NavbarSetting() {
   return (
@@ -29,13 +32,21 @@ export default function NavbarSetting() {
       </Grid>
       {/* AVATAR */}
       <Grid alignItems="center" justify="center">
-        <Avatar
-          size="lg"
-          src={defaultAvatar}
-          color="gradient"
-          bordered
-          css={{ cursor: "pointer" }}
-        />
+        {/* <ProfileOptions /> */}
+        <Tooltip
+          content={<ProfileOptions />}
+          placement="bottomEnd"
+          css={tooltipCss}
+          hideArrow
+        >
+          <Avatar
+            size="lg"
+            src={defaultAvatar}
+            color="gradient"
+            bordered
+            css={{ cursor: "pointer" }}
+          />
+        </Tooltip>
       </Grid>
     </Grid.Container>
   );
