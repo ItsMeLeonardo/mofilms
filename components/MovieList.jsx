f01291dc2d0ce6ac4333c26b69bdaca66b345d07
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-21 13:48:24 -0500

    [FIX] add the correct props to `movieCard`

--- components/MovieList.jsx
+++ components/MovieList.jsx
@@ -1,15 +1,20 @@
 import { Grid } from "@nextui-org/react";
+
 import MovieCard from "./MovieCard";
+import { formatDate } from "../utils/formatDate";
+import { formatImageUrl } from "../services/formatImageUrl";
 
 export default function MovieList({ movies = [] } = []) {
   return (
     <Grid.Container gap={2} justify="flex-start">
-      {movies.map((movie, index) => (
+      {movies.map((movie) => (
         <MovieCard
-          {...movie}
-          key={`${movie.title}-${index}-${Date.now()}`}
+          key={movie.id}
+          title={movie.title}
+          date={formatDate(movie.date, { dateStyle: "medium" })}
+          rate={movie.vote_average}
+          poster={formatImageUrl(movie.poster_path)}
           h={350}
-          cols={3}
           className="movieList-item"
         />
       ))}
