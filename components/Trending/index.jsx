3b247997ec8a58602660ac8e6c153460254dc0cc
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-02-20 19:58:50 -0500

    [FIX] replace static data to dynamic props

--- components/Trending/index.jsx
+++ components/Trending/index.jsx
@@ -3,79 +3,33 @@ import { TwoUsers } from "react-iconly";
 
 import HorizontalList from "../HorizontalList";
 import MovieCard from "../MovieCard";
-const data = [
-  {
-    title: "The Shawshank Redemption",
-    year: 1994,
-    poster:
-      "https://64.media.tumblr.com/9c26a4dcfea433c46a36a2ae57f34e29/1d81862d4bac390e-5b/s500x750/6b8853732877309a1176d207c18112fcdb0f4b91.jpg",
-    rate: 9.3,
-    date: "1994-10-14",
-    backdropImg:
-      "https://images.unsplash.com/photo-1590666027616-38e0283eb6c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmVvbiUyMGdpcmx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    title: "The Godfather",
-    year: 1972,
-    poster:
-      "https://64.media.tumblr.com/a42cae9d62d982dbb65412201bdf0665/1d81862d4bac390e-48/s1280x1920/6a994756bb2fa131796ba504f63bc80cbc64f9b5.jpg",
-    rate: 9.2,
-    date: "1972-03-14",
-  },
-  {
-    title: "The Godfather: Part II",
-    year: 1974,
-    poster:
-      "https://64.media.tumblr.com/b758278adda46dfd5b9946b4d4a96e9b/a6b000c88aa082e3-14/s1280x1920/9fc33042e985d211ac79eeaea2f3d135b264c4c9.jpg",
-    rate: 9.0,
-    date: "1974-12-20",
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    poster:
-      "https://64.media.tumblr.com/c6f7a91dfd9258cc09f9fbc878c3999d/a6b000c88aa082e3-eb/s1280x1920/c0b2301c43c173226bb03b3fedcb0938241371c9.jpg",
-    rate: 8.9,
-    date: "2008-07-18",
-  },
-  {
-    title: "The Shawshandsdsk Redemption",
-    year: 1994,
-    poster:
-      "https://64.media.tumblr.com/9c26a4dcfea433c46a36a2ae57f34e29/1d81862d4bac390e-5b/s500x750/6b8853732877309a1176d207c18112fcdb0f4b91.jpg",
-    rate: 9.3,
-    date: "1994-10-14",
-  },
-  {
-    title: "The Dark Knight 2",
-    year: 2008,
-    poster:
-      "https://64.media.tumblr.com/c6f7a91dfd9258cc09f9fbc878c3999d/a6b000c88aa082e3-eb/s1280x1920/c0b2301c43c173226bb03b3fedcb0938241371c9.jpg",
-    rate: 8.9,
-    date: "2008-07-18",
-  },
-];
 
-const Badge = () => (
+import { formatDate } from "../../utils/formatDate";
+import { formatImageUrl } from "../../services/formatImageUrl";
+
+const Badge = ({ popularity }) => (
   <>
     <TwoUsers stroke="bold" size="small" />
     <Text size={12} weight="bold">
-      1000
+      {popularity}
     </Text>
   </>
 );
 
-export default function MostPopular() {
+export default function MostPopular({ movies = [] } = {}) {
   return (
     <>
       <h1>Trending</h1>
       <HorizontalList>
-        {data.map((movie, index) => (
+        {movies.map((movie) => (
           <MovieCard
-            {...movie}
-            rank={index + 1}
-            key={`${movie.title} + ${Math.random}`}
-            badge={<Badge />}
+            key={movie.id}
+            title={movie.title}
+            rate={movie.vote_average}
+            poster={formatImageUrl(movie.poster_path)}
+            date={formatDate(movie.release_date, { dateStyle: "medium" })}
+            backdropImg={formatImageUrl(movie.backdrop_path)}
+            badge={<Badge popularity={movie.vote_count} />}
           />
         ))}
       </HorizontalList>
