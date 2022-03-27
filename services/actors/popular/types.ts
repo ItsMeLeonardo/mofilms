5ef9399dbeccb3010bde7d574a30c25a47b9982a
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-27 12:39:55 -0500

    FEAT: modularize the `Result` field in actors

--- services/actors/popular/types.ts
+++ services/actors/popular/types.ts
@@ -1,22 +1,7 @@
-import { KnownFor } from "services/actors/types/knowFor";
+import { GenericActor } from "services/actors/types";
 export interface ActorPopularResponse {
   page: number;
-  results: Result[];
+  results: GenericActor[];
   total_pages: number;
   total_results: number;
-}
-
-export interface Result {
-  adult: boolean;
-  gender: number;
-  id: number;
-  known_for: KnownFor[];
-  known_for_department: KnownForDepartment;
-  name: string;
-  popularity: number;
-  profile_path: string;
-}
-
-export enum KnownForDepartment {
-  Acting = "Acting",
 }
--- services/actors/search/types.ts
+++ services/actors/search/types.ts
@@ -1,19 +1,8 @@
-import { KnownFor } from "services/actors/types/knowFor";
+import { GenericActor } from "services/actors/types";
 
 export interface ActorSearchResponse {
   page: number;
-  results: Result[];
+  results: GenericActor[];
   total_pages: number;
   total_results: number;
-}
-
-export interface Result {
-  adult: boolean;
-  gender: number;
-  id: number;
-  known_for: KnownFor[];
-  known_for_department: string;
-  name: string;
-  popularity: number;
-  profile_path: null | string;
 }
