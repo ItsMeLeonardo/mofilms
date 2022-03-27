06d44b45f7824bfc7dfda5d66f9c2921121181f8
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-27 17:23:19 -0500

    REFACTOR-TS: add types to profile options

--- components/Profile/ProfileOptions.tsx
+++ components/Profile/ProfileOptions.tsx
@@ -1,16 +1,18 @@
-import { Button, Row } from "@nextui-org/react";
+import { Button, Row, CSS } from "@nextui-org/react";
 import { Setting, User, Logout } from "react-iconly";
+
+const buttonCss: CSS = { w: "100%" };
 
 export default function ProfileOptions() {
   return (
     <Row css={{ flexDirection: "column", gap: "0.5rem" }}>
-      <Button icon={<Setting />} size="sm" light css={{ w: "100%" }}>
+      <Button icon={<Setting />} size="sm" light css={buttonCss}>
         Settings
       </Button>
-      <Button icon={<User />} size="sm" light css={{ w: "100%" }}>
+      <Button icon={<User />} size="sm" light css={buttonCss}>
         Profile
       </Button>
-      <Button icon={<Logout />} size="sm" light css={{ w: "100%" }}>
+      <Button icon={<Logout />} size="sm" light css={buttonCss}>
         Logout
       </Button>
     </Row>
