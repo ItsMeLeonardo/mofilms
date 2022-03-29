1725225ec536314f390496c2d3ec928349a66dc1
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-03-29 18:31:24 -0500

    RESPONSIVE: organize styles and quality image

--- components/KeywordResults.tsx
+++ components/KeywordResults.tsx
@@ -1,5 +1,5 @@
 import LinkNext from "next/link";
-import { Button, Text, Avatar, Spacer } from "@nextui-org/react";
+import { Button, Text, Avatar, Spacer, CSS } from "@nextui-org/react";
 //utils
 import imageService from "services/images";
 //types
@@ -8,6 +8,21 @@ import { Result } from "services/movies/search/types";
 interface Props {
   results: Result[];
 }
+
+//nextui css
+const resultButtonCss: CSS = {
+  bg: "$accents1",
+  justifyContent: "flex-start",
+  my: ".5rem",
+  "&:hover": { bg: "$gray900" },
+};
+
+const resultTextCss: CSS = {
+  bg: "transparent",
+  maxW: "17ch",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+};
 
 export default function KeywordResult({ results }: Props) {
   if (results?.length === 0 || !results) {
@@ -24,26 +39,10 @@ export default function KeywordResult({ results }: Props) {
     <>
       {results.map(({ id, title, poster_path }) => (
         <LinkNext key={id} href={`/movies/${id}`}>
-          <Button
-            css={{
-              bg: "$accents1",
-              justifyContent: "flex-start",
-              my: ".5rem",
-              "&:hover": { bg: "$gray900" },
-            }}
-            as="a"
-          >
-            <Avatar src={imageService.poster.w185(poster_path)} squared />
+          <Button css={resultButtonCss} as="a">
+            <Avatar src={imageService.poster.w92(poster_path)} squared />
             <Spacer />
-            <Text
-              h6
-              css={{
-                bg: "transparent",
-                maxW: "17ch",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
+            <Text h6 css={resultTextCss}>
               {title}
             </Text>
           </Button>
