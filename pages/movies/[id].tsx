0a0aa010f92d0df4cfd42cd747b91218490a0b7f
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-04-09 19:38:10 -0500

    FEAT: add dynamic import to movie list

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,14 +1,16 @@
 import { GetServerSideProps } from "next";
 import { useState, useEffect } from "react";
 import Head from "next/head";
+import dynamic from "next/dynamic";
 
 import { Text, Grid } from "@nextui-org/react";
 //components
 import DetailPoster from "components/DetailPoster";
-import MovieList from "components/MovieList";
 import MovieCast from "components/MovieDetail/MovieCast";
 import MovieData from "components/MovieDetail/MovieData";
 import MovieOptions from "components/MovieDetail/MovieOptions";
+import MovieListLoader from "components/MovieList/loaders";
+const MovieList = dynamic(() => import("components/MovieList"));
 //utils
 import movieService from "services/movies";
 
@@ -74,7 +76,12 @@ export default function MovieDetails({ movie }: MovieDetailProps) {
       <span ref={similarMoviesRef}>
         {/* this is a flag to call the similar movies */}
       </span>
-      {similarMovies && <MovieList movies={similarMovies} />}
+
+      {similarMovies ? (
+        <MovieList movies={similarMovies} />
+      ) : (
+        <MovieListLoader />
+      )}
     </>
   );
 }
